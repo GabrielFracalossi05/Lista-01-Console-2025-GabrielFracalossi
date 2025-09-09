442e317a64b7670080454dfbff6ede379d828a8b
Gabriel Fracalossi <0064211@sesi-es.org.br>
2025-09-09 09:22:01 -0300

    Finalizado

--- Atividade 7/Atividade 7/Program.cs
+++ Atividade 7/Atividade 7/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Gabriel");
+Console.WriteLine("Digite um numero");
+int numer = int.Parse(Console.ReadLine());
+if (numer > 10)
+{
+    Console.WriteLine("É MAIOR QUE 10");
+}
+else
+{
+    Console.WriteLine("É MENOR QUR 10");
+}
