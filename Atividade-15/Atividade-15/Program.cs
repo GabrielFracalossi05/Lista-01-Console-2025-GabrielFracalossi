db7b462266b3bc7d06ff189fc1f47443ebe67936
Gabriel Fracalossi <0064211@sesi-es.org.br>
2025-09-17 08:59:34 -0300

    Atividade 15 Concluida

---
+++ Atividade-15/Atividade-15/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double resultado;
+for (int contador = 1000; contador <= 2000; contador++) 
+{
+    resultado = contador % 11;
+    if (resultado == 5);
+    {
+        Console.WriteLine($"{contador}");
+    }
+}
