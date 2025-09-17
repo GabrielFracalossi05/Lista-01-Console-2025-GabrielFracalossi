88d3f77fdf0b6b1711070be03bb938dd37aa2df2
Gabriel Fracalossi <0064211@sesi-es.org.br>
2025-09-17 08:27:56 -0300

    Atividade 14 CONCLUIDA

---
+++ Atividade-14/Atividade-14/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Escreva um numemero maior que 0");
+int num = int.Parse(Console.ReadLine());
+for (int contador = 1; contador < num; contador++)
+{
+    Console.WriteLine($"{contador}");
+}
